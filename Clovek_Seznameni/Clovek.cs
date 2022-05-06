aad848f15226bea2d6d7e4727338d05a15363fa1
Pepa Pánek <josef.panek@gmail.com>
2022-05-06 15:07:40 +0200

    Add project files.

---
+++ Clovek_Seznameni/Clovek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clovek_Seznameni
+{
+    /// <summary>
+    /// Třída reprezentuje člověka
+    /// </summary>
+    internal class Clovek
+    {
+        /// <summary>
+        /// Celé jméno
+        /// </summary>
+        public string jmeno;
+        /// <summary>
+        /// Věk
+        /// </summary>
+        public int vek;
+        /// <summary>
+        /// Kamarád
+        /// </summary>
+        public Clovek kamarad;
+
+        /// <summary>
+        /// Vypíše text, ve kterém se človek představí
+        /// </summary>
+        public void PredstavSe()
+        {
+            Console.WriteLine("Ahoj, já jsem {0}, je mi {1} let a můj kamarád je {2}", jmeno, vek, kamarad.jmeno);
+        }
+    }
+}
---
+++ Clovek_Seznameni/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clovek_Seznameni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vytvoření lidí
+            Clovek karel = new Clovek();
+            karel.jmeno = "Karel Novák";
+            karel.vek = 33;
+            Clovek josef = new Clovek();
+            josef.jmeno = "Josef Nový";
+            josef.vek = 27;
+            // Spřátelení
+            karel.kamarad = josef;
+            josef.kamarad = karel;
+            // Představení
+            karel.PredstavSe();
+            josef.PredstavSe();
+            Console.ReadKey();
+        }
+    }
+}
